5b919d17ed54019df6f64cb96da7543001b85c71
ko4ereg <rosadchiy@yandex.ru>
2024-05-21 23:30:12 +0500

    фывфыв

--- src/components/common/ButtonRemoveFromCart/ButtonRemoveFromCart.jsx
+++ src/components/common/ButtonRemoveFromCart/ButtonRemoveFromCart.jsx
@@ -1,5 +1,9 @@
 import s from './ButtonRemoveFromCart.module.scss';
 import cartminus from './../../../assets/icons/cartminus.svg';
+
+
+const img = new Image();
+img.src = cartminus;
 const ButtonRemoveFromCart = ({ disabled, text, onClick }) => {
   return (
     <button onClick={onClick} className={s.button} disabled={disabled}>
