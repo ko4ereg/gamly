40a1548ba35314c365683e348bbeb24533ec12c9
ko4ereg <rosadchiy@yandex.ru>
2024-05-27 20:33:14 +0500

    as

--- src/components/common/ButtonSecondary/ButtonSecondary.jsx
+++ src/components/common/ButtonSecondary/ButtonSecondary.jsx
@@ -1,8 +1,8 @@
 import s from './ButtonSecondary.module.scss';
 
-const ButtonSecondary = ({ disabled, text, onClick, icon }) => {
+const ButtonSecondary = ({ disabled, text, onClick, icon, small }) => {
   return (
-    <button onClick={onClick} className={s.button} disabled={disabled}>
+    <button onClick={onClick} className={`${s.button} ${small ? s.small : ''}`} disabled={disabled}>
       {icon}
       {text}
     </button>
