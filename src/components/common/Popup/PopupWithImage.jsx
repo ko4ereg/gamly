4ba345975df5dfc928830a74821c710a9e4fe653
ko4ereg <rosadchiy@yandex.ru>
2024-06-07 20:53:37 +0500

    asas

--- src/components/common/Popup/PopupWithImage.jsx
+++ src/components/common/Popup/PopupWithImage.jsx
@@ -37,7 +37,7 @@ const PopupWithImage = ({ popupActive, setPopupActive, children, heading, note, 
             <div className={s.popup_container} >
                 <div className={s.top_container}
                     // style={{ backgroundImage: `url(${image})`  }} 
-                    style={{ backgroundImage: ` url(${image}) ` }}
+                    style={{ backgroundImage: `linear-gradient(0deg, rgba(20, 17, 26, 0.4) 0%, rgba(20, 17, 26, 0.4) 100%),   url(${image})`    }}
                 >
                     <CloseButton icon={<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <g id="Frame">
