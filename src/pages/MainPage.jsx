4f61ce81e73550a15324c0bafedfecf974d8ff92
ko4ereg <rosadchiy@yandex.ru>
2024-06-21 19:51:53 +0500

    addlenis

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,22 +1,27 @@
 import HeadingBlock from "../components/MainPage/HeadingBlock/HeadingBlock";
-
+import { ReactLenis, useLenis } from 'lenis/react'
 import g from './../Globalstyles.module.css';
 import s from './MainPage.module.css';
 import BottomBlock from "../components/MainPage/BottomBlock/BottomBlock";
-import Paragraph1 from "../components/MainPage/Paragraph/Text";
+ 
 import Paragraph from "../components/MainPage/TextBlock/Paragraph";
 
 
 const MainPage = (props) => {
 
-
+    const lenis = useLenis(({ scroll }) => {
+        // called every scroll
+      })
+    
     return (
+        <ReactLenis root>
         <div className={g.container + ' ' + s.container}>
             <HeadingBlock />
             {/* <TextBlock/> */}
             <Paragraph />
             <BottomBlock />
-        </div>)
+        </div>
+        </ReactLenis>)
 }
 
 export default MainPage;
