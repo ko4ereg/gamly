0d9426e4a5c7babf94dc7395e37edfbe4058125e
ko4ereg <rosadchiy@yandex.ru>
2024-06-25 22:18:37 +0500

    fixmain

--- src/components/BigFooter/BigFooter.jsx
+++ src/components/BigFooter/BigFooter.jsx
@@ -5,13 +5,15 @@ import MenuColumn from './MenuColumn/MenuColumn';
 
 const BigFooter = () => {
     return (
-        <div className={s.container}>
+       
+            <div className={s.container}>
             <div className={s.top}>
               <LinksColumn /> 
               <MenuColumn /> 
             </div>
            <div className={s.bottom}> <MiniFooter/></div>
             <div className={s.backEllipse} ></div>
+          
         </div>
     )
 }
--- src/components/MainPage/HeadingBlock/HeadingBlock.jsx
+++ src/components/MainPage/HeadingBlock/HeadingBlock.jsx
@@ -8,11 +8,15 @@ const HeadingBlock = (props) => {
     const { scrollYProgress } = useScroll();
     const scrollRef = useRef(null);
     const translateY = useTransform(scrollYProgress, [0, 1], [0,  100]); 
+    const { scrollY } = useScroll(); // Отслеживаем значение Y-скролла
+    const opacity = useTransform(scrollY, [0, 400], [0.34, 0]);
     return (
         <section className={s.headingBlock}>
             <Heading />
             <HeadingFeatures />
-            <motion.div  style={{ translateY }}
+            <motion.div  
+            // style={{  translateY }}
+            style={{  opacity:  opacity   }}
         ref={scrollRef}  className={s.backEllipse} />
         </section>)
 }
